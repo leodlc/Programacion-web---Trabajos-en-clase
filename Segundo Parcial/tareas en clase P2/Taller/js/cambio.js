35c10ab11c658f746f368b649a512e6f42efa69d
leodlc <ljde@espe.edu.ec>
2022-06-29 11:36:21 -0500

    taller 29 de junio 20200

---
+++ Segundo Parcial/tareas en clase P2/Taller/js/cambio.js
@@ -0,0 +1,9 @@
+function azul(){
+    document.getElementById("caja").style.background="blue";
+}
+function amarillo(){
+    document.getElementById("caja").style.background="yellow";
+}
+function rojo(){
+    document.getElementById("caja").style.background="red";
+}
