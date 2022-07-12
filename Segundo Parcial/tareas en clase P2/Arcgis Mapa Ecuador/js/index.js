ecc887a4d1171d3f20d8fcc9338ac8bb8dcdee8d
leodlc <ljde@espe.edu.ec>
2022-07-12 01:24:10 -0500

    segunda version-mapa ecuador

--- Segundo Parcial/tareas en clase P2/Arcgis Mapa Ecuador/js/index.js
+++ Segundo Parcial/tareas en clase P2/Arcgis Mapa Ecuador/js/index.js
@@ -1,3 +1,20 @@
+class ProvinciaCollection{
+    constructor(items){
+        this.items=items
+    }
+    first(){
+        return new ProvinciaPath(this.items[0])
+    }
+    find(index){
+        return new ProvinciaPath(this.items[index])
+    }
+    last(){
+
+    }
+    static get(){
+        return new ProvinciaCollection(document.querySelectorAll("path"))
+    }
+}
 class ProvinciaPath{
     constructor(element){
         this.element=element
@@ -15,6 +32,4 @@ class ProvinciaPath{
     }
     
 }
-
-ProvinciaPath.findByName("imbabura")
-
+/* console.log(ProvinciaCollection.get().find(2).fill("red")) */
